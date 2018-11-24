b60d178ad4674558aa8abec0e56a59d73b299f1a
Jing Dong <jingdong@192-168-1-9.tpgi.com.au>
2018-11-24 16:28:23 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,28 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardTwo);*/
+
+
+
+cards = ['queen', 'queen', 'king', 'king'];
+cardsInPlay = [];
+
+var cardOne = cards [0];
+var cardTwo = cards [2];
+cardsInPlay.push (cardOne);
+cardsInPlay.push (cardTwo);
+console.log(" User flipped " + cardOne);
+console.log(" User flipped " + cardTwo);
+
+
+if (cardsInPlay[0] === cardsInPlay[2]){
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
